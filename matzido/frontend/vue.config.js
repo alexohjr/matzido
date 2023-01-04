9be1f345f9bda7f77e3d78afbed9368ffa159c8e
ohch <alexohjr@naver.com>
2023-01-04 14:39:33 +0900

    프로젝트 초기 세팅 - vue연동

---
+++ matzido/frontend/vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir : "../src/main/resources/static",
+  devServer : {
+    port : 3001,
+    proxy : {
+        '/api' : {
+            target : 'http://localhost:8083',
+            changeOrigin: true
+        }
+    }
+  }
+})
