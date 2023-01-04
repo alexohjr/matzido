14be440c27b80ecb8b1c68f161856449ab6f808d
ohch <alexohjr@naver.com>
2023-01-04 10:44:18 +0900

    프로젝트 초기 세팅

---
+++ matzido/src/test/java/com/matdoring/matzido/MatzidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matdoring.matzido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
