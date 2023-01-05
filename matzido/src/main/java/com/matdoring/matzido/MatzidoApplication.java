107280d182a25e0368d2a73b824a23165c1f550d
OhChangHwan <ohchanghwan@OhChangHwans-MacBook-Pro.local>
2023-01-05 16:50:12 +0900

    aop 로그 구현

--- matzido/src/main/java/com/matdoring/matzido/MatzidoApplication.java
+++ matzido/src/main/java/com/matdoring/matzido/MatzidoApplication.java
@@ -2,9 +2,13 @@ package com.matdoring.matzido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+
 @EnableAspectJAutoProxy
+@EnableConfigurationProperties
 @SpringBootApplication
 public class MatzidoApplication {
 
--- matzido/src/main/java/com/matdoring/matzido/aop/LoggingAspect.java
+++ matzido/src/main/java/com/matdoring/matzido/aop/LoggingAspect.java
@@ -1,5 +1,80 @@
 package com.matdoring.matzido.aop;
 
 
+import com.google.common.base.Joiner;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.*;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("within(com.matdoring.matzido..*)")
+    public Object logging(ProceedingJoinPoint pjp) throws Throwable {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String params = getRequestParams();
+
+        long startAt = System.currentTimeMillis();
+
+        logger.info("----------> REQUEST : {}({}) = {}{}",
+                pjp.getSignature().getDeclaringTypeName(),
+                pjp.getSignature().getName(), params,
+                request.getRequestURL());
+
+        logger.info("##### requestUrl : {}", request.getRequestURL());
+
+        Object result = pjp.proceed();
+
+        long endAt = System.currentTimeMillis();
+
+        logger.info("##### response : {}({}) = {} ({}ms)", pjp.getSignature().getDeclaringTypeName(),
+                pjp.getSignature().getName(), result, endAt-startAt);
+
+        return result;
+
+    }
+
+    // get requset value
+    private String getRequestParams() {
+
+        String params = "";
+
+        RequestAttributes requestAttribute = RequestContextHolder.getRequestAttributes();
+
+        if(requestAttribute != null){
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes()).getRequest();
+
+            Map<String, String[]> paramMap = request.getParameterMap();
+
+            if(!paramMap.isEmpty()) {
+                params = " [" + paramMapToString(paramMap) + "]";
+            }
+        }
+        return params;
+    }
+
+    private String paramMapToString(Map<String, String[]> paramMap) {
+        return paramMap.entrySet().stream()
+                .map(entry -> String.format("%s -> (%s)",
+                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
+                .collect(Collectors.joining(", "));
+    }
+
 }
